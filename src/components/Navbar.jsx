603d1c3dbe592ccb8d61d1f3a13325ff7246407d
TheyKay <murraykyleb@gmail.com>
2021-06-02 10:32:33 -0600

    added links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,17 @@ const Navbar = () => {
   return (
     <header>
       <nav>
-
+        <ul>
+          <li>
+            <a href="/account">account</a>
+          </li>
+          <li>
+            <a href="/library">library</a>
+          </li>
+          <li>
+            <a href="/browse">browse</a>
+          </li>
+        </ul>
       </nav>
     </header>
   )
