6ca166b8f24df8fa02288c6dffb303235ddaf8d7
TheyKay <murraykyleb@gmail.com>
2021-06-03 17:14:39 -0600

    started styling, moved pages into dedicated directories so can use coordinated scss, updated refs

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
-export { default as Account } from './Account';
-export { default as Browse } from './Browse';
-export { default as Default } from './Default';
-export { default as Episode } from './Episode';
-export { default as Library } from './Library';
-export { default as Settings } from './Settings';
-export { default as Show } from './Show';
+export { default as Account } from './Account/Account';
+export { default as Browse } from './Browse/Browse';
+export { default as Default } from './Default/Default';
+export { default as Episode } from './Episode/Episode';
+export { default as Library } from './Library/Library';
+export { default as Settings } from './Settings/Settings';
+export { default as Show } from './Show/Show';
