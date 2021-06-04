5cc5c89998c2a45b55a6f68553fa00a9750004cc
TheyKay <murraykyleb@gmail.com>
2021-06-04 10:25:48 -0600

    styling navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,27 +1,22 @@
-import { useLocation } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { MdAccountCircle } from 'react-icons/md';
 import { FaSearch } from 'react-icons/fa';
 import { BsCollectionFill } from 'react-icons/bs';
 import './Navbar.scss';
-
-const usePathName = () => {
-  const location = useLocation();
-  return location.pathname;
-}
 
 const Navbar = () => {
   return (
     <header id="navbar">
       <nav>
         <ul className="menu-links">
-          <li className={usePathName() === '/browse' ? 'menu-links-item current' : "menu-links-item"}>
-            <a className="menu-link" href="/browse"><FaSearch /></a>
+          <li className="menu-links-item">
+            <NavLink to='browse' className="menu-link" activeClassName="current"><FaSearch /></NavLink>
           </li>
-          <li className={usePathName() === '/library' ? 'menu-links-item current' : "menu-links-item"}>
-            <a className="menu-link" href="/library"><BsCollectionFill /></a>
+          <li className="menu-links-item">
+            <NavLink to='library' className="menu-link" activeClassName="current"><BsCollectionFill /></NavLink>
           </li>
-          <li className={usePathName() === '/account' ? 'menu-links-item current' : "menu-links-item"}>
-            <a className="menu-link" href="/account"><MdAccountCircle /></a>
+          <li className="menu-links-item">
+            <NavLink to='account' className="menu-link" activeClassName="current"><MdAccountCircle /></NavLink>
           </li>
         </ul>
       </nav>
